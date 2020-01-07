483f925cb676639b51be30c4e82759260b9363b3
marconimp <marconimp@yahoo.com.br>
2020-01-07 20:37:06 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+dpkg -i flussonic-erlang_18.0.1_amd64.deb
+dpkg -i flussonic-python_2.7.3_amd64.deb
+dpkg -i flussonic-ffmpeg_4.1_amd64.deb
+dpkg -i flussonic_4.5.18_amd64.deb
+
+# H@C|<
+
+cp -r hack/siphoned/* /opt/flussonic/apps/online/ebin/
+cp hack/online/online.app /opt/flussonic/apps/online/ebin/online.app
+cp hack/online/license4.beam /opt/flussonic/apps/online/ebin/license4.beam
+cp hack/online/license_agent.beam /opt/flussonic/apps/online/ebin/license_agent.beam
+
+echo "flu starts with the following command:  /etc/init.d/flussonic run"
